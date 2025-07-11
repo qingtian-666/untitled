2ae7efa9bba76e32557c163bbeb803d081de46b6
qingtian-666 <1419853316@qq.com>
2025-07-11 11:58:44 +0800

    pull commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,7 +12,8 @@ public class Main {
         System.out.println("hello git3");
         System.out.println("hello git4");
         System.out.println("hello push");
-        System.out.println("push test");
+        System.out.println("push testddd");
+        System.out.println("pull testddd");
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
